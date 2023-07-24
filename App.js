f6d0285528e9e7f2ad746ab52a622ff8fd6ebeb6
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-07-24 19:47:58 +0700

    provide ui to create data for local notification

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import { StyleSheet, ToastAndroid, View } from 'react-native';
+import { Container, Content, Form, Item, Label, Button, Input, Text, NativeBaseProvider, FormControl, Box } from 'native-base';
 export default function App() {
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [data, setData] = useState('');
+  const [seconds, setSeconds] = useState('');
+
+  async function scheduleLocalNotification() {
+    if (title.trim() === '' || body.trim() === '' || seconds.trim() === '') {
+      ToastAndroid.show('Please fill all fields', ToastAndroid.SHORT);
+      return;
+    }
+
+    ToastAndroid.show(`Title: ${title}, Body: ${body}, seconds: ${seconds}`, ToastAndroid.SHORT);
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Container safeArea padding={5}>
+        <>
+          <>
+            <FormControl>
+              <FormControl.Label>Title:</FormControl.Label>
+              <Input value={title} onChangeText={setTitle} />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Body:</FormControl.Label>
+              <Input value={body} onChangeText={setBody} />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Data (optional):</FormControl.Label>
+              <Input value={data} onChangeText={setData} />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Trigger in seconds</FormControl.Label>
+              <Input
+                value={seconds}
+                onChangeText={setSeconds}
+                keyboardType="numeric"
+              />
+            </FormControl>
+          </>
+
+          <Button onPress={scheduleLocalNotification} mt={10} width={'full'}>
+            Set Notification
+          </Button>
+        </>
+      </Container>
+    </NativeBaseProvider>
   );
 }
 
